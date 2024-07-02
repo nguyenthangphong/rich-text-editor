1dcceefa1d3fd5998cc590e9a781d2e6f99b0d6c
nthangphong2601 <nthangphong2601@gmail.com>
2024-07-02 23:51:17 +0700

    File IO - Done

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QCloseEvent>
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QFileDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +20,45 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_triggered();
+    void on_actionOpen_triggered();
+    void on_actionSave_triggered();
+    void on_actionSave_As_triggered();
+    void on_actionExit_triggered();
+    void on_actionCopy_triggered();
+    void on_actionCut_triggered();
+    void on_actionPaste_triggered();
+    void on_actionFind_triggered();
+    void on_actionReplace_triggered();
+    void on_actionSelect_All_triggered();
+    void on_actionZoom_in_triggered();
+    void on_actionZoom_out_triggered();
+    void on_actionBold_triggered();
+    void on_actionItalic_triggered();
+    void on_actionUnderline_triggered();
+    void on_actionStrike_triggered();
+    void on_actionColor_triggered();
+    void on_actionFont_triggered();
+    void on_actionHelp_triggered();
+    void on_actionAbout_triggered();
+
+    void on_textEdit_textChanged();
+
 private:
     Ui::MainWindow *ui;
+    bool m_changed;
+    QString m_path;
+
+    void newFile();
+    void openFile();
+    void saveFile(QString path);
+    void SaveFileAs();
+    void checkSave();
+
+
+    // QWidget interface
+protected:
+    void closeEvent(QCloseEvent *event) override;
 };
 #endif // MAINWINDOW_H
